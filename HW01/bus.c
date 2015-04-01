e140a2c4ba9fbb5eaa098ae1f87949211afb7cdf
sonicyang <sonic.tw.tp@gmail.com>
2015-04-01 12:23:14 +0800

    fix bug on ghost entering station
    add losts of command

--- HW01/bus.c
+++ HW01/bus.c
@@ -23,12 +23,14 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+/* 用來保存資料並代表Stack的Sturcture */
 typedef struct mStack{
     int buffer[1024];
     int *sp;
     int count;
 }mStack_t;
 
+/* 用來保存資料並代表Queue的Sturcture */
 typedef struct mQueue{
     int buffer[1024];
     int *head;
@@ -36,16 +38,21 @@ typedef struct mQueue{
     int count;
 }mQueue_t;
 
+/* 一些Stack的基本操作 */
+/* 第一個參數傳入的是要操作的Stack */
 void stack_init(mStack_t *stack);
 int stack_push(mStack_t *stack, int data);
 int stack_pop(mStack_t *stack);
 int stack_isEmpty(mStack_t *stack);
 
+/* 一些Queue的基本操作 */
+/* 第一個參數傳入的是要操作的Queue */
 void queue_init(mQueue_t *queue);
 int queue_push(mQueue_t *queue, int data);
 int queue_pop(mQueue_t *queue);
 int queue_isEmpty(mQueue_t *queue);
 
+/* 測資 */
 int data[32] = {70, 67, 75, 55, \
                 51, 40, 87, 65, \
                 58, 86, 66, 43, \
@@ -54,52 +61,69 @@ int data[32] = {70, 67, 75, 55, \
                 };
 
 int main(void){
-    int i, guysOnBus;
-    int totalWeight, buf;
+    int i, guysOnBus; // 計數器
+    int totalWeight, buf; // 目前車上重量, 暫存區
 
-    mStack_t bus;
-    mQueue_t inBusStop;
-    mQueue_t notInBusStop;
-    mQueue_t destination;
+    mStack_t bus; //公車
+    mQueue_t inBusStop; //車站
+    mQueue_t notInBusStop; //未進入車站的人
+    mQueue_t destination; //電機系館
 
+    /* 初始化 */
     stack_init(&bus);
     queue_init(&inBusStop);
     queue_init(&notInBusStop);
     queue_init(&destination);
 
+    /* 載入測資*/
     for(i = 0; i < 20; i++){
         queue_push(&notInBusStop, data[i]);
     }
 
+    /* 先讓初使的4個人進站 */
     for(i = 0; i < 4; i ++){
         buf = queue_pop(&notInBusStop);
         printf("A guy with %dkg enter the station\n", buf);
         queue_push(&inBusStop, buf);
     }
+
+    /* 再繫感有20個人以前 */
     while(destination.count < 20){
+
+        
+        /* 再讓4個人進站, 總共8個 */
         for(i = 0; i < 4; i ++){
-            buf = queue_pop(&notInBusStop);
-            printf("A guy with %dkg enter the station\n", buf);
-            queue_push(&inBusStop, buf);
+            if(!queue_isEmpty(&notInBusStop)){
+                buf = queue_pop(&notInBusStop);
+                printf("A guy with %dkg enter the station\n", buf);
+                queue_push(&inBusStop, buf);
+            }
         }
 
+        /* 開始跟具體重上車, 車上不到四人，持續嘗試 */
         totalWeight = 0;
         for(guysOnBus = 0; guysOnBus < 4;){
+
+            /* 先上車 */
             buf = queue_pop(&inBusStop);
             totalWeight += buf;
             printf("A guy with %dkg try to get on the bus\n", buf);
             
+            /* 檢查是否太胖 */
             if(totalWeight > 260){
+                /* 歧視胖子, 把胖子趕下車, 排到隊伍後面*/
                 totalWeight -= buf;
                 queue_push(&inBusStop, buf);
                 printf("The guy with %dkg is too fat. Thus, leaving the bus\n", buf);
             }else{
+                /* 沒事, 已上車 */
                 stack_push(&bus, buf);
                 printf("The guy with %dkg is on bus.\n", buf);
                 guysOnBus++;
             }
         }
 
+        /* 到達系館, 下車 */
         for(i = 0; i < 4; i++){
             buf = stack_pop(&bus); 
             printf("A guy with %dkg arrived at destination\n", buf);
@@ -112,6 +136,7 @@ int main(void){
     return 0;
 }
 
+/* Stack 初始化 */
 void stack_init(mStack_t *stack){
     stack->sp = stack->buffer;
     stack->count = 0;
@@ -119,29 +144,40 @@ void stack_init(mStack_t *stack){
     return;
 }
 
+/* Stack Push */
 int stack_push(mStack_t *stack, int data){
+
+    /* 檢查是否會 Overflow */
     if(stack->count >= 1024){
         return 1;
     }
+
+    /* 存入資料, 移動頂端位置 */
     *(++stack->sp) = data;
     stack->count++;
 
     return 0;
 }
 
+/* Stack Pop */
 int stack_pop(mStack_t *stack){
+
+    /* 檢查是否會 Underflow */
     if(stack->count == 0){
         return 0;
     }
     stack->count--;
 
+    /* 讀取資料, 移動頂端位置 */
     return *(stack->sp--);
 }
 
+/* 檢查 Stack是否為空 */
 int stack_isEmpty(mStack_t *stack){
     return !(stack->count);
 }
 
+/* Queue初始化 */
 void queue_init(mQueue_t *queue){
     queue->tail = queue->head = queue->buffer;
     queue->count = 0;
@@ -149,10 +185,14 @@ void queue_init(mQueue_t *queue){
     return;
 }
 
+/* Queue push */
 int queue_push(mQueue_t *queue, int data){
+    /* 檢查是否會 Overflow */
     if(queue->count >= 1024){
         return 1;
     }
+
+    /* 存入資料, 移動前端位置, 使用RingBuffer實作 */
     *(queue->head++) = data;
     if(queue->head >= queue->buffer + 1024){
         queue->head = queue->buffer;
@@ -163,14 +203,17 @@ int queue_push(mQueue_t *queue, int data){
     return 0;
 }
 
+/* Queue Pop */
 int queue_pop(mQueue_t *queue){
     int ret;
 
+    /* 檢查是否會 Underflow */
     if(queue->count == 0){
         return 0;
     }
     queue->count--;
 
+    /* 讀取資料, 移動尾端位置, 使用RingBuffer實作 */
     ret =  *(queue->tail++);
 
     if(queue->tail >= queue->buffer + 1024){
@@ -180,6 +223,7 @@ int queue_pop(mQueue_t *queue){
     return ret;
 }
 
+/* 檢查 Queue 是否為空 */
 int queue_isEmpty(mQueue_t *queue){
     return !(queue->count);
 }
